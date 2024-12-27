345f4c290207a2df3c2159088059d43fee3ac45f
Jack <jack4mccgmail.comgit config --global user.email jack4mccgmail.com>
2024-12-27 15:38:58 +0100

    week four lab 6 redone

--- Lab 6 Temp Tables.sql
+++ Lab 6 Temp Tables.sql
@@ -1,14 +1,23 @@
 USE sakila;
 
-CREATE VIEW customer_summary2 AS
-SELECT email, COUNT(rental_id), customer_id
-FROM customer c
-INNER JOIN rental r
-ON c.customer_id = r.customer_id
-GROUP BY email;
 
-SELECT *
-FROM customer_summary2;
+CREATE VIEW customer_summary3 AS
+SELECT 
+    c.customer_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    c.email,
+    COUNT(*) AS rental_count
+FROM 
+    customer AS c
+    JOIN rental AS r ON c.customer_id = r.customer_id
+GROUP BY 
+    c.customer_id
+ORDER BY 
+    rental_count DESC;
+    
+    SELECT *
+    FROM customer_summary3;
+
 
 SELECT *
 FROM rental;
@@ -16,19 +25,30 @@ FROM rental;
 SELECT *
 FROM payment;
 
-CREATE TEMPORARY TABLE amount_paid AS
-SELECT email, SUM(amount) AS total_paid
-FROM customer c
+CREATE TEMPORARY TABLE customer_payment_summary AS (
+SELECT cs.customer_id, email, SUM(amount) AS total_paid
+FROM customer_summary3 cs
 INNER JOIN payment p
-ON c.customer_id = p.customer_id
-GROUP BY email;
+ON cs.customer_id = p.customer_id
+GROUP BY cs.customer_id);
 
-SELECT *
-FROM amount_paid;
 
-WITH combined_table AS (
-SELECT email, SUM(amount) AS total_paid, last_name, email, amount
-FROM amount_paid am)
-INNER JOIN customer_summary2 c2
-ON amm_customer_id = c2.customer_id;
+WITH customer_summary_report AS(
+SELECT 
+    cs.customer_name,
+    cs.email,
+    cs.rental_count,
+    cs.total_paid
+FROM 
+    customer_summary3 AS cs
+    JOIN customer_summary3 AS cps ON cs.customer_id = cps.customer_id)
+
+SELECT 
+    *,
+    total_paid / rental_count AS average_payment_per_rental
+FROM 
+    customer_summary_report
+ORDER BY 
+    rental_count DESC;
+
 
